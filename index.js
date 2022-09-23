35feb55fb86881f226e85d419f3c9efe517b90c8
marianaceotto <marianaceott@gmail.com>
2022-09-23 15:39:11 -0300

    Semana3

--- index.js
+++ index.js
@@ -2,50 +2,79 @@
 //FILMES/SÉRIES
 
 const serie1 = {
-     nome: "A Maldição",
-     ano: 2018,
-     temporadas: 2,
-     producao: false,
-     genero: ["Terror", "Drama", "Ficção Supernatural"]
- }
+  nome: "A Maldição",
+  ano: 2018,
+  temporadas: 2,
+  producao: false,
+  genero: ["Terror", "Drama", "Ficção Supernatural"],
+};
 
 const serie2 = {
-     nome: "Criminal Minds",
-     ano: 2005, 
-     temporadas: 15,
-     producao: false,
-     genero: ["Drama", "Policial", "Suspense", "Ação"]
-}
+  nome: "Criminal Minds",
+  ano: 2005,
+  temporadas: 15,
+  producao: false,
+  genero: ["Drama", "Policial", "Suspense", "Ação"],
+};
 
 const serie3 = {
-    nome: "The Big Bang Theory",
-     ano: 2007,
-    temporadas: 12,
-    producao: false,
-    genero: ["Comédia","Sitcom"]
-}
+  nome: "The Big Bang Theory",
+  ano: 2007,
+  temporadas: 12,
+  producao: false,
+  genero: ["Comédia", "Sitcom"],
+};
 
-console.log(serie1.nome.toUpperCase(), serie1)
-console.log(serie2.nome.toUpperCase(), serie2)
-console.log(serie3.nome.toUpperCase(), serie3)
+// console.log(serie1.nome.toUpperCase(), serie1)
+// console.log(serie2.nome.toUpperCase(), serie2)
+// console.log(serie3.nome.toUpperCase(), serie3)
 
 //Imprimir média das temporadas
-const media = (serie1.temporadas + serie2.temporadas + serie3.temporadas) /2
-console.log("A média de temporadas é:", media)
+//const media = (serie1.temporadas + serie2.temporadas + serie3.temporadas) / 2;
+//console.log("A média de temporadas é:", media)
 
-
-console.log("As séries 1, 2 e 3 continuam sendo produzidas?", serie1.producao && serie2.producao && serie3.producao)
-
+//console.log("As séries 1, 2 e 3 continuam sendo produzidas?", serie1.producao && serie2.producao && serie3.producao)
 
 //----------------------------------------------
 
-const todasAsSeries = []
+const todasAsSeries = [];
 
-if (serie1 !== serie2 !== serie3) {
-   todasAsSeries.push(serie1, serie2, serie3)
-    } else {
-      console.log(alert("Sua série ainda não foi adicionada"))  
+if ((serie1 !== serie2) !== serie3) {
+  todasAsSeries.push(serie1, serie2, serie3);
+} else {
+  console.log(alert("Sua série ainda não foi adicionada"));
+}
+
+console.log(todasAsSeries);
+
+//----------------------------------------------
+
+//  for (i in todasAsSeries){
+//    console.log(todasString[i])
+// }
+
+function todasStrings (series){
+    for (i in series){
+      console.log(`${i}: ${series[i]}\n`)
     }
+    console.log('------------------------------')
+}
+
+todasStrings(serie1)
+todasStrings(serie2)
+todasStrings(serie3)
 
 
-
+function encontraSerie(array, string){
+  const buscaSerie = array.filter((elemento) => {
+      if(elemento.nome === string){
+          return elemento
+   }
+ })
+  if(buscaSerie.length === 0){
+      alert("Série não encontrada")
+  }else{
+      console.log(buscaSerie);
+  }
+}
+//encontraSerie(todasAsSeries, "The Big Bang Theory")
